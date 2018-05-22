f560660ab3ceaeece5c070a656c1f743145be337
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-05-22 17:52:49 -0300

    addEventListener()

--- eventListener.js
+++ eventListener.js
@@ -1,16 +1,16 @@
 var btn = document.getElementById('btn');
 
-	btn.addEventListener('click', funcao1);
-	btn.addEventListener('click', funcao2);
-	btn.removeEventListener('click', funcao1);
-	btn.addEventListener('click', funcao3);
+btn.addEventListener('click', funcao1);
+btn.addEventListener('click', funcao2);
+btn.removeEventListener('click', funcao1);
+btn.addEventListener('click', funcao3);
 
-	function funcao1() {
-		alert('alert - 1');
-	}
-	function funcao2() {
-		alert('alert - 2');
-	}
-	function funcao3() {
-		alert('alert - 3');
-	}
+function funcao1() {
+	alert('alert - 1');
+}
+function funcao2() {
+	alert('alert - 2');
+}
+function funcao3() {
+	alert('alert - 3');
+}
