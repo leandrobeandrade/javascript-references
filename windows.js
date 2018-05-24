86215367b4f5446af18624daa570e6a33f84ebca
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-05-24 18:19:35 -0300

    window.location

--- windows.js
+++ windows.js
@@ -1,8 +1,8 @@
 function teste(){
 	alert('-' +window.location.href+ '\n' +
-		'-' +window.location.hostname+ '\n' +
-		'-' +window.location.pathname+ '\n' +
-		'-' +window.location.protocol+ '\n' +
-		'-' +window.location.port
+	      '-' +window.location.hostname+ '\n' +
+	      '-' +window.location.pathname+ '\n' +
+	      '-' +window.location.protocol+ '\n' +
+	      '-' +window.location.port
 	);
 }		//window.location.assign("https://www.google.com");
