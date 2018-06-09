418328787c5ae34434ebe3bce3db92c9c8599a55
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-06-09 19:56:26 -0300

    Tipo de dados - TS

--- typescript/scripts/ts01.ts
+++ typescript/scripts/ts01.ts
@@ -5,12 +5,12 @@ document.body.appendChild(p);
 
 /*-------------*/
 
-let numer: number = 10;										// tipo NUMBER
-let boole: boolean = true;									// tipo BOOLEAN
-let nulos: null;											// tipo NULL
-let indef: undefined;										// tipo UNDEFINED
-let annys: any = 'tyufsd'+ 10;								// tipo ANY - qualquer tipo
-let text1: string = 'Testando';								// tipo STRING		
+let numer: number = 10;										              // tipo NUMBER
+let boole: boolean = true;									            // tipo BOOLEAN
+let nulos: null;											                  // tipo NULL
+let indef: undefined;										                // tipo UNDEFINED
+let annys: any = 'tyufsd'+ 10;							            // tipo ANY - qualquer tipo
+let text1: string = 'Testando';							            // tipo STRING		
 let text2: string = "Testando";							
 let text3: string = `${text2}`;							
 
@@ -25,13 +25,13 @@ console.log(text3);
 
 /*-------------*/
 
-function func(): void {}									// tipo VOID
+function func(): void {}									              // tipo VOID
 
 console.log(func);
 
 /*-------------*/
 
-let lista1: number[] = [1,2,3];								// tipo VETOR
+let lista1: number[] = [1,2,3];							            // tipo VETOR
 let lista2: Array<number> = [1,2,3];
 
 console.log(lista1);
@@ -39,7 +39,7 @@ console.log(lista2);
 
 /*-------------*/
 
-let tupla: [string, number];								// tipo TUPLA
+let tupla: [string, number];								            // tipo TUPLA
 tupla = ['Qualquer texto!', 2018];
 
 console.log(tupla[0].length);
