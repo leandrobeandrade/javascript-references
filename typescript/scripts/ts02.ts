54241027f9335d0ef2bd60bfcb2300971c13c772
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-06-12 10:01:05 -0300

    tipos de dados - TS

--- typescript/scripts/ts02.ts
+++ typescript/scripts/ts02.ts
@@ -1,6 +1,6 @@
 let teste: any= "Javascript";
 
-let comprim: number = (<string>teste).length;									// (<string>) força para tipo string
+let comprim: number = (<string>teste).length;						// (<string>) força para tipo string
 let tamanho: string = (teste as string).toUpperCase();
 
 console.log(comprim);
@@ -8,7 +8,7 @@ console.log(tamanho);
 
 /*-------------*/
 
-function num(v1:number, v2:number, v3?:number): number {						// ? indica que o argumento é opcional
+function num(v1:number, v2:number, v3?:number): number {				// ? indica que o argumento é opcional
 	if (v3 != undefined) {
 		return v1 + v2 + v3;
 	}
@@ -19,7 +19,7 @@ console.log(num(10,10,10));
 
 /*-------------*/
 
-function test(v1: any, v2:any, v3?:any): any {									// retorno sem saber o tipo
+function test(v1: any, v2:any, v3?:any): any {						// retorno sem saber o tipo
 	if (v3 != undefined) {
 		return v1 + v2 + v3;
 	}
@@ -31,7 +31,7 @@ console.log(`retorno com string: ${test('Fulano ','Ciclano ','Beltrano')}`);
 
 /*-------------*/
 
-function add(v1:string, ...v2:number[]): void {									// ... rest parameter
+function add(v1:string, ...v2:number[]): void {						// ... rest parameter
 	let soma: number = 0;
 	for (var i=0; i<v2.length; i++) {
 		soma += v2[i];
