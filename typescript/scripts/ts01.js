3901f237baf26bfa82fb67fb12f7cffe67e4dc12
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-06-13 09:30:24 -0300

    tipo de dados - JS

--- typescript/scripts/ts01.js
+++ typescript/scripts/ts01.js
@@ -46,4 +46,4 @@ var dia1 = Dia.SEGUNDA; // tipo ENUM
 var dia2 = Dia[1];
 console.log(dia1);
 console.log(dia2);
-console.log(Dia.QUARTA + " | " + Dia.QUINTA);
+console.log(Dia.QUARTA + " | " + Dia.QUINTA); 
