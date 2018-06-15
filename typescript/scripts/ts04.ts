a5a6c4bfe92310fb75988ce9e5f748680ee346ef
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-06-15 10:20:11 -0300

    classes - TS

--- typescript/scripts/ts04.ts
+++ typescript/scripts/ts04.ts
@@ -1,5 +1,5 @@
 class Pessoa {
-	public nome: string;												// modificador de acesso public
+	public nome: string;							// modificador de acesso public
 	constructor(nome:string) {
 		this.nome = nome;
 	}
@@ -9,7 +9,7 @@ class Pessoa {
 }
 
 class Empregado  extends Pessoa {
-	private salario: number;											// modificador de acesso protected
+	private salario: number;						// modificador de acesso protected
 	constructor(nome:string, salario:number){
 		super(nome);
 		this.salario = salario;
@@ -26,12 +26,12 @@ let emp1 = new Empregado('Beltrano', 2500);
 pes1.print();
 emp1.print();
 
-let pes2 = {nome: 'Ciclano'} as Pessoa;									// cria uma pessoa de Pessoa
+let pes2 = {nome: 'Ciclano'} as Pessoa;						// cria uma pessoa de Pessoa
 console.log(pes2);
 
 let pes3 = <Pessoa>{};
-pes3.nome = 'Belciclano';												// cria um pessoa de Pessoa
+pes3.nome = 'Belciclano';							// cria um pessoa de Pessoa
 console.log(pes3);
 
-let pes4: Pessoa = new Empregado('Ted',3500);							// polimorfismo
+let pes4: Pessoa = new Empregado('Ted',3500);					// polimorfismo
 pes4.print();
