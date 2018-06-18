b91a065c34c6eba083977bde2bcf38fc70897069
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-06-18 09:19:41 -0300

    compatibilidade de tipos - JS

--- typescript/scripts/ts07.js
+++ typescript/scripts/ts07.js
@@ -10,24 +10,24 @@ var outroExemplo2 = /** @class */ (function () {
 }());
 var exemplo1;
 console.log(exemplo1 = new outroExemplo1());
-// console.log(exemplo = new outroExemplo2());									erro
+// console.log(exemplo = new outroExemplo2());                                  erro
 var exemplo2 = { nome: 'Fulano', idade: 34 };
 exemplo1 = exemplo2;
 console.log(exemplo1);
-// exemplo1 = {nome:'Fulano', idade:34};										erro
+// exemplo1 = {nome:'Fulano', idade:34};                                        erro
 function testeExemplo1(ex) {
     console.log(ex.nome);
 }
 var exemplo3 = { nome: 'Ciclano', idade: 34 };
 testeExemplo1(exemplo3);
 testeExemplo1({ nome: 'Fulano' });
-//testeExemplo1({nome:'Fulano', idade: 34});									erro
+//testeExemplo1({nome:'Fulano', idade: 34});                                    erro
 var x1 = function (a) { return a; };
 var x2 = function (b, c) { return b + c; };
 var x3 = function (b, c) { return b; };
 console.log(10);
 console.log(10, 'teste');
-//x2 = x1;																		erro - x2 tem mais argumentos
+//x2 = x1;                                                                      erro - x2 tem mais argumentos
 x3 = x1;
 console.log(x3(50, 'qualquer coisa'));
 var itens = [1, 2, 3];
