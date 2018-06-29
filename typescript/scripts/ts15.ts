bb2933e58f75fbede91be9b1c87ce019995114d2
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-06-29 09:31:59 -0300

    desestruturação - TS

--- typescript/scripts/ts15.ts
+++ typescript/scripts/ts15.ts
@@ -1,23 +1,23 @@
 let pessoa = {nome:'Fulano', sobrenome:'Beltrano'};
 let nome1 = pessoa;
 let nome2 = pessoa.nome;
-let nome3 = pessoa.sobrenome;									// Acesso normal
+let nome3 = pessoa.sobrenome;					// Acesso normal
 
 console.log(nome1);
 console.log(nome2);
 console.log(nome3);
 
-let {nome:nomes,sobrenome:sobrenomes} = pessoa;							// Destruturação
+let {nome:nomes,sobrenome:sobrenomes} = pessoa;			// Desestruturação
 console.log(nomes, '-', sobrenomes);
 
 
 
 let vetor = ['Javascript', 'Typescript'];
 
-console.log(vetor[0]);										// Acesso normal
+console.log(vetor[0]);						// Acesso normal
 console.log(vetor[1]);
 
-let [a,b] = vetor;										// Destruturação
+let [a,b] = vetor;						// Desestruturação
 console.log(a,'-', b);
 
 
@@ -29,13 +29,13 @@ class Pessoa {
 }
 
 function exemplo1(pessoa: Pessoa): void {
-	console.log(pessoa.nome);								// Acesso normal
+	console.log(pessoa.nome);				// Acesso normal
 }
 
 exemplo1(new Pessoa('Fulano'));
 
 function exemplo2({nome}): void {
-	console.log(nome);									// Destruturação
+	console.log(nome);					// Desestruturação
 }
 
 exemplo2(new Pessoa('Beltrano'));
