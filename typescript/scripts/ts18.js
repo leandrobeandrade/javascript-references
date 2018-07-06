bb0542a227ee35ebb158ef297b838971c3163920
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-07-06 14:07:19 -0300

    set - JS

--- typescript/scripts/ts18.js
+++ typescript/scripts/ts18.js
@@ -11,5 +11,5 @@ console.log(nomes2.has('Ciclano'));
 for (var _i = 0, nomes2_1 = nomes2; _i < nomes2_1.length; _i++) {
     var aux = nomes2_1[_i];
     console.log(aux);
-}
+} 
 nomes2.forEach(function (value) { return console.log(value); });
