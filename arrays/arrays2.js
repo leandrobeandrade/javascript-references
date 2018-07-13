4807f3062cf9834c7a8e6fb22da3507efdaf1d41
Leandro Bezerra de Andrade <leandrobe.andrade@gmail.com>
2018-07-13 09:43:21 -0300

    arrays2

--- arrays/arrays2.js
+++ arrays/arrays2.js
@@ -1,12 +1,12 @@
 var times1 = ['Ceará', 'Grêmio', 'Vasco', 'Atlético-MG']; 
 var times2 = ['Santos', 'Cruzeiro'];
 		
-var teste1 = times1.pop(); // retira o ultimo elemento do vetor
+var teste1 = times1.pop(); 			// retira o ultimo elemento do vetor
 times1.push('Atlético-MG', 'Bahia');
-var teste2 = times1.shift(); // retira o primeiro elemento do vetor
-times1.unshift('Palmeiras'); // adiciona um elemento no primeiro indice[0] do vetor
-times1.splice(1,0, 'Ceará'); // adiciona um elemento(1) e não retira nenhum(0) do vetor
-times1.splice(1,1) // retira o segundo elemento do vetor
+var teste2 = times1.shift(); 			// retira o primeiro elemento do vetor
+times1.unshift('Palmeiras'); 			// adiciona um elemento no primeiro indice[0] do vetor
+times1.splice(1,0, 'Ceará'); 			// adiciona um elemento(1) e não retira nenhum(0) do vetor
+times1.splice(1,1) 				// retira o segundo elemento do vetor
 var teste3 = times1.concat(times2);
 var teste4 = times1.slice(3);
 var teste5 = times1.reverse();
