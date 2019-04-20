9d2b81295652fb3a2a5870f33c2e944a9502c772
Leandro B Andrade <leandrobe.andrade@gmail.com>
2019-04-20 15:47:11 -0300

    Arrays

--- arrays/arrays3.js
+++ arrays/arrays3.js
@@ -7,6 +7,7 @@ var carros = [
 /* ===============================================
 |			FILTER      		  |
 ================================================*/
+Cria um novo array com todos os elementos que passaram no teste implementado pela função fornecida.
 
 var filtra = carros.filter(function(elemento) {
 	return elemento.marca === 'Ford'
@@ -14,9 +15,12 @@ var filtra = carros.filter(function(elemento) {
 
 console.log(filtra)
 
+
+
 /* ===============================================
 |			MAP    			  |
 ================================================*/
+Invoca a função callback passada por argumento para cada elemento do Array e devolve um novo Array como resultado.
 
 var mapeia = carros.map(function(elemento) {
 	return elemento.modelo.length
@@ -24,9 +28,12 @@ var mapeia = carros.map(function(elemento) {
 
 console.log(mapeia)
 
+
+
 /* ===============================================
 |			REDUCE   		  |
 ================================================*/
+Executa uma função reducer (provida por você) para cada membro do array, resultando num único valor de retorno.
 
 var carro = [
 	{marca: 'Ford', preco: 28800},
@@ -34,14 +41,18 @@ var carro = [
 	{marca: 'Fiat', preco: 32000},
 ];
 
-carro.reduce(function(anterior, atual) {
+var reduz = carro.reduce(function(anterior, atual) {
 	return anterior + atual.preco
 },0)
+
+console.log(reduz)
+
 
 
 /* ===============================================
 |			FOREACH   	          |
 ================================================*/
+Executa uma dada função em cada elemento de um array.
 
 var total = 0;
 
