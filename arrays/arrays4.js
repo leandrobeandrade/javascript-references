b1c1fc6ec879f01fb5261a3074b1afbbcf366732
Leandro B Andrade <leandrobe.andrade@gmail.com>
2019-06-18 09:33:05 -0300

    Arrays

--- arrays/arrays4.js
+++ arrays/arrays4.js
@@ -1,17 +1,26 @@
-deixa nosso array plano recursivamente em uma profundidade especificada como argumento, ou seja, é uma operação que concatena os elementos de um array.[1, 2, [3, 4]].flat(); // [ 1, 2, 3, 4] 
-[1, 2, [3, 4, [5, 6]]].flat(); // [ 1, 2, 3, 4, [5, 6]]
+/* ===============================================
+|			FLAT      		  |
+================================================*/
+
+Deixa o Array plano recursivamente em uma profundidade especificada como argumento, ou seja, 
+é uma operação que concatena os elementos de um array.
+
+      [1, 2, [3, 4]].flat();   // [ 1, 2, 3, 4] 
+      [1, 2, [3, 4, [5, 6]]].flat();   // [ 1, 2, 3, 4, [5, 6]]
 
 Passando o número 2 como argumento a função fica plana em 2 níveis.
-[1, 2, [3, 4, [5, 6]]].flat(2);
-// [ 1, 2, 3, 4, 5, 6 ]
-Exemplos
-const array1 = [1, 2, 3]
-const array2 = [4, 5, 6]
-array1.push(...array2) //...spread
-array1 // [1, 2, 3, 4, 5, 6]
-const array3 = [
- …array1,
- …array2
-] // [1, 2, 3, 4, 5, 6]
-com flat()
-[array1, array2].flat()
+
+      [1, 2, [3, 4, [5, 6]]].flat(2);   // [ 1, 2, 3, 4, 5, 6 ]
+
+Exemplos: 
+      
+      const array1 = [1, 2, 3];
+      const array2 = [4, 5, 6];
+      
+      // SEM flat():
+      array1.push(...array2);    // [1, 2, 3, 4, 5, 6]
+
+      const array3 = [...array1, ...array2];   // [1, 2, 3, 4, 5, 6]
+
+      // COM flat():
+      [array1, array2].flat();
