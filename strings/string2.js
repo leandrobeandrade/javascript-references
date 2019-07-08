944819d5cf7029916706d484869f6ad323929833
Leandro B Andrade <leandrobe.andrade@gmail.com>
2019-07-08 09:03:11 -0300

    Strings

--- strings/string2.js
+++ strings/string2.js
@@ -3,9 +3,9 @@ var texto2 = 1000;
 
 var teste1 = texto1.slice(9, 19);
 var teste2 = texto1.slice(44);
-var teste3 = teste1.concat(" ", teste2); // concatena uma string com outra
+var teste3 = teste1.concat(' ', teste2); // concatena uma string com outra
 var teste4 = texto1.charAt(51);
-var teste5 = texto1.split(",");
+var teste5 = texto1.split(',');
 var teste6 = texto2.toString();
 var teste7 = texto1.repeat(5) // repete uma string |*ECMASCRIPT6*|
 
