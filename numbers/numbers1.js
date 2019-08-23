1a2a48507204ddee111eddcf5d0d33795a54a89a
Leandro B Andrade <leandrobe.andrade@gmail.com>
2019-08-23 08:53:59 -0300

    Numbers

--- numbers/numbers1.js
+++ numbers/numbers1.js
@@ -19,7 +19,7 @@ console.log(teste3);                  // '1.000e+3'
 console.log(numero2);                 // 10.3
 console.log(teste4);                  // '10'
 console.log(teste5);                  // '10.30'
-console.log(teste6);
+console.log(teste6);                  // '10.300'
 console.log(teste7);
 console.log(teste8);
 console.log(teste9);
